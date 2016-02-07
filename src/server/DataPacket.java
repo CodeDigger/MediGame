fa2adad65f99f54a08f309ec2c55a2d6564da218
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2016-02-07 23:29:37 +0100

    added data packet class

---
+++ src/server/DataPacket.java
@@ -0,0 +1,12 @@
+package server;
+
+/**
+ * Created by Tobias on 2016-02-07.
+ * This is the class for the network packet
+ * sent between server and client
+ */
+public class DataPacket {
+    
+
+
+}
